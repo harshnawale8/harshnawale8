15ccefc9c643bb8abc3f66afd282c553c65eaebe
Harsh Nawale <146943630+harshnawale8@users.noreply.github.com>
2023-10-17 23:33:48 +0530

    Update 29 A (Display Prime Numbers Between Two Intervals)C Program to Display Prime Numbers Between Two Intervals.c

--- 29 A (Display Prime Numbers Between Two Intervals)C Program to Display Prime Numbers Between Two Intervals.cpp
+++ 29 A (Display Prime Numbers Between Two Intervals)C Program to Display Prime Numbers Between Two Intervals.cpp
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
    int low, high, i, flag;
    printf("Enter two numbers(intervals): ");
    scanf("%d %d", &low, &high);
    printf("Prime numbers between %d and %d are: ", low, high);
 
-   // iteration until low is not equal to high
    while (low < high) {
       flag = 0;
 
-      // ignore numbers less than 2
       if (low <= 1) {
          ++low;
          continue;
       }
 
-      // if low is a non-prime number, flag will be 1
       for (i = 2; i <= low / 2; ++i) {
 
          if (low % i == 0) {
@@ -28,8 +26,6 @@ int main() {
       if (flag == 0)
          printf("%d ", low);
 
-      // to check prime for the next number
-      // increase low by 1
       ++low;
    }
 
