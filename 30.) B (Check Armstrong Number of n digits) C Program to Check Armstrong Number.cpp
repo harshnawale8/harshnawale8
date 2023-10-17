734ec2d83ac9390e07297212a0e41045bab2d900
Harsh Nawale <146943630+harshnawale8@users.noreply.github.com>
2023-10-17 23:35:37 +0530

    Update 30.) B (Check Armstrong Number of n digits) C Program to Check Armstrong Number.cpp

--- 30.) B (Check Armstrong Number of n digits) C Program to Check Armstrong Number.cpp
+++ 30.) B (Check Armstrong Number of n digits) C Program to Check Armstrong Number.cpp
@@ -10,7 +10,6 @@ int main() {
 
    originalNum = num;
 
-   // store the number of digits of num in n
    for (originalNum = num; originalNum != 0; ++n) {
        originalNum /= 10;
    }
@@ -18,11 +17,9 @@ int main() {
    for (originalNum = num; originalNum != 0; originalNum /= 10) {
        remainder = originalNum % 10;
 
-      // store the sum of the power of individual digits in result
       result += pow(remainder, n);
    }
 
-   // if num is equal to result, the number is an Armstrong number
    if ((int)result == num)
     printf("%d is an Armstrong number.", num);
    else
