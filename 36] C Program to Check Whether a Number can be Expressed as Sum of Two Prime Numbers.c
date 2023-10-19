b24c4f34ea6646540d36af4578803bb8b585dc5d
Harsh Nawale <146943630+harshnawale8@users.noreply.github.com>
2023-10-19 22:48:25 +0530

    36] C Program to Check Whether a Number can be Expressed as Sum of Two Prime Numbers.c

--- 36] C Program to Check Whether a Number can be Expressed as Sum of Two Prime Numbers.c
+++ 36] C Program to Check Whether a Number can be Expressed as Sum of Two Prime Numbers.c
@@ -6,9 +6,7 @@ int main() {
   scanf("%d", &n);
 
   for (i = 2; i <= n / 2; ++i) {
-    // condition for i to be a prime number
     if (checkPrime(i) == 1) {
-      // condition for n-i to be a prime number
       if (checkPrime(n - i) == 1) {
         printf("%d = %d + %d\n", n, i, n - i);
         flag = 1;
@@ -22,11 +20,9 @@ int main() {
   return 0;
 }
 
-// function to check prime number
 int checkPrime(int n) {
   int i, isPrime = 1;
 
-  // 0 and 1 are not prime numbers
   if (n == 0 || n == 1) {
     isPrime = 0;
   }
