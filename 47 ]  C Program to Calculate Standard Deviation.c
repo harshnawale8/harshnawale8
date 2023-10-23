ac89cd5897d9211eeac193e3a3a278c49555fa5f
Harsh Nawale <146943630+harshnawale8@users.noreply.github.com>
2023-10-23 20:59:03 +0530

    47 ]  C Program to Calculate Standard Deviation.c

--- 47 ]  C Program to Calculate Standard Deviation.c
+++ 47 ]  C Program to Calculate Standard Deviation.c
@@ -1,8 +1,8 @@
-// SD of a population
-#include <math.h>
-#include <stdio.h>
+#include<math.h>
+#include<stdio.h>
 float calculateSD(float data[]);
-int main() {
+int main() 
+{
     int i;
     float data[10];
     printf("Enter 10 elements: ");
