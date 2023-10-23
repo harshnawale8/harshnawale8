b019a932aed23d6c9a17230b45b140e0a210b095
Harsh Nawale <146943630+harshnawale8@users.noreply.github.com>
2023-10-23 20:59:51 +0530

    Update 48 ]  C Program to Add Two Matrices Using Multi-dimensional Arrays.c

--- 48 ]  C Program to Add Two Matrices Using Multi-dimensional Arrays.cpp
+++ 48 ]  C Program to Add Two Matrices Using Multi-dimensional Arrays.cpp
@@ -1,5 +1,6 @@
-#include <stdio.h>
-int main() {
+#include<stdio.h>
+int main()
+{
   int r, c, a[100][100], b[100][100], sum[100][100], i, j;
   printf("Enter the number of rows (between 1 and 100): ");
   scanf("%d", &r);
@@ -20,13 +21,11 @@ int main() {
       scanf("%d", &b[i][j]);
     }
 
-  // adding two matrices
   for (i = 0; i < r; ++i)
     for (j = 0; j < c; ++j) {
       sum[i][j] = a[i][j] + b[i][j];
     }
-
-  // printing the result
+  
   printf("\nSum of two matrices: \n");
   for (i = 0; i < r; ++i)
     for (j = 0; j < c; ++j) {
