1f9218ddd464693c674f359e72f787326eff42c6
Harsh Nawale <146943630+harshnawale8@users.noreply.github.com>
2023-10-27 23:57:22 +0530

    60 ] C Program to Copy String Without Using strcpy ( ).c

--- 60 ] C Program to Copy String Without Using strcpy ( ).c
+++ 60 ] C Program to Copy String Without Using strcpy ( ).c
@@ -1,10 +1,13 @@
-#include <stdio.h>
-int main() {
+#include<stdio.h>
+int main()
+{
     char s1[100], s2[100], i;
     printf("Enter string s1: ");
     fgets(s1, sizeof(s1), stdin);
 
-    for (i = 0; s1[i] != '\0'; ++i) {
+    for (i = 0; s1[i] != '\0'; ++i) 
+    
+    {
         s2[i] = s1[i];
     }
 
