aeea23c7ef174f3142cf51d744ea608746b455e2
Harsh Nawale <146943630+harshnawale8@users.noreply.github.com>
2023-10-29 17:39:32 +0530

    61] C Program to Sort Elements in Lexicographical Order (Dictionary Order).c

--- 61] C Program to Sort Elements in Lexicographical Order (Dictionary Order).c
+++ 61] C Program to Sort Elements in Lexicographical Order (Dictionary Order).c
@@ -1,21 +1,21 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
-int main() {
+int main()
+   {
    char str[5][50], temp[50];
    printf("Enter 5 words: ");
 
-   // Getting strings input
    for (int i = 0; i < 5; ++i) {
       fgets(str[i], sizeof(str[i]), stdin);
    }
 
-   // storing strings in the lexicographical order
    for (int i = 0; i < 5; ++i) {
-      for (int j = i + 1; j < 5; ++j) {
+      for (int j = i + 1; j < 5; ++j)
+      {
 
-         // swapping strings if they are not in the lexicographical order
-         if (strcmp(str[i], str[j]) > 0) {
+         if (strcmp(str[i], str[j]) > 0)
+         {
             strcpy(temp, str[i]);
             strcpy(str[i], str[j]);
             strcpy(str[j], temp);
