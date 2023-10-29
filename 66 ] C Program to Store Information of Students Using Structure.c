36b5482c23d8705114d85921286512f8aa0cfb62
Harsh Nawale <146943630+harshnawale8@users.noreply.github.com>
2023-10-29 17:44:49 +0530

    66 ] C Program to Store Information of Students Using Structure.c

--- 66 ] C Program to Store Information of Students Using Structure.c
+++ 66 ] C Program to Store Information of Students Using Structure.c
@@ -1,16 +1,18 @@
-#include <stdio.h>
-struct student {
+#include<stdio.h>
+struct student
+{
     char firstName[50];
     int roll;
     float marks;
 } s[5];
 
-int main() {
+int main()
+{
     int i;
     printf("Enter information of students:\n");
 
-    // storing information
-    for (i = 0; i < 5; ++i) {
+    for (i = 0; i < 5; ++i)
+    {
         s[i].roll = i + 1;
         printf("\nFor roll number%d,\n", s[i].roll);
         printf("Enter first name: ");
@@ -20,7 +22,6 @@ int main() {
     }
     printf("Displaying Information:\n\n");
 
-    // displaying information
     for (i = 0; i < 5; ++i) {
         printf("\nRoll number: %d\n", i + 1);
         printf("First name: ");
