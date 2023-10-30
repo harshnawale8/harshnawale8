2f58d8591762f89a555478a954e00011c090b7b6
Harsh Nawale <146943630+harshnawale8@users.noreply.github.com>
2023-10-30 22:00:15 +0530

    69 ] C Program to Read the First Line From a File.c

--- 69 ] C Program to Read the First Line From a File.c
+++ 69 ] C Program to Read the First Line From a File.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h> // For exit() function
-int main() {
+int main()
+{
     char c[1000];
     FILE *fptr;
-    if ((fptr = fopen("program.txt", "r")) == NULL) {
+    if ((fptr = fopen("program.txt", "r")) == NULL)
+    {
         printf("Error! File cannot be opened.");
-        // Program exits if the file pointer returns NULL.
         exit(1);
     }
-
-    // reads text until newline is encountered
     fscanf(fptr, "%[^\n]", c);
     printf("Data from the file:\n%s", c);
     fclose(fptr);
