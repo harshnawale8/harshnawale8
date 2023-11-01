01de449f357d8479a62ce2138924d9dbfaa4c45d
Harsh Nawale <146943630+harshnawale8@users.noreply.github.com>
2023-11-01 19:16:01 +0530

    79 ] ( Pascal's Triangle)C Program to Print Pyramids and Patterns.c

--- 79 ] ( Pascal's Triangle)C Program to Print Pyramids and Patterns.c
+++ 79 ] ( Pascal's Triangle)C Program to Print Pyramids and Patterns.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
-int main() {
+#include<stdio.h>
+int main()
+{
    int rows, coef = 1, space, i, j;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
-   for (i = 0; i < rows; i++) {
+   for (i = 0; i < rows; i++)
+   {
       for (space = 1; space <= rows - i; space++)
          printf("  ");
-      for (j = 0; j <= i; j++) {
+      for (j = 0; j <= i; j++)
+      {
          if (j == 0 || i == 0)
             coef = 1;
          else
