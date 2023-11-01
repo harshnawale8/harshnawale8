77e462c0ce38b018b7c083a539a0301c10e16bdb
Harsh Nawale <146943630+harshnawale8@users.noreply.github.com>
2023-11-01 19:16:40 +0530

    80 ] ( Floyd's Triangle)C Program to Print Pyramids and Patterns.c

--- 80 ] ( Floyd's Triangle)C Program to Print Pyramids and Patterns.c
+++ 80 ] ( Floyd's Triangle)C Program to Print Pyramids and Patterns.c
@@ -1,10 +1,13 @@
-#include <stdio.h>
-int main() {
+#include<stdio.h>
+int main()
+{
    int rows, i, j, number = 1;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
-   for (i = 1; i <= rows; i++) {
-      for (j = 1; j <= i; ++j) {
+   for (i = 1; i <= rows; i++)
+   {
+      for (j = 1; j <= i; ++j)
+      {
          printf("%d ", number);
          ++number;
       }
